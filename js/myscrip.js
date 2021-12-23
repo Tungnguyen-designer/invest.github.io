f462cc2607e057d02cb8a801b845a24590b280a5
Tungnguyen-designer <95853220+Tungnguyen-designer@users.noreply.github.com>
2021-12-23 16:17:54 +0700

    update 2.4

--- js/myscrip.js
+++ js/myscrip.js
@@ -1,6 +1,7 @@
+//Button back to top
 //Get the button
 var mybutton = document.getElementById("myBtn");
-    
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -12,6 +13,11 @@ function scrollFunction() {
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
